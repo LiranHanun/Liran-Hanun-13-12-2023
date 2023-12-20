184bb4718542406de4a87bf0fdc974bdd16b6de2
Liran <liran302010@gmail.com>
2023-12-20 22:54:45 +0200

    first commit

--- src/HomeF/Home.js
+++ src/HomeF/Home.js
@@ -32,11 +32,12 @@ const Home =(props)=>{
     // Getting city information from api
     const getWetherDetails =(cityName) => {
         if (!cityName) return
-        const apiURL = "/locations/v1/cities/search?apikey="+apiKey+"&q=" + cityName+""
+        const apiURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey="+apiKey+"&q=" + cityName+""
+        console.log(apiURL)
         axios.get(apiURL).then((res) => {
           const cityKey= res.data[0].Key
           setDataCityName(res.data[0])
-          const apiURLtwo= "/forecasts/v1/daily/5day/"+cityKey+"?apikey="+apiKey+""
+          const apiURLtwo= "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+cityKey+"?apikey="+apiKey+""
           axios.get(apiURLtwo).then((res)=>{
 
             setdataCityInfo(res.data)
@@ -64,7 +65,7 @@ const Home =(props)=>{
       
       
       useEffect(() => { 
-        const apiUrl = "/locations/v1/cities/geoposition/search?apikey="+apiKey+"=31.99663%2C34.82840"
+        const apiUrl = "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey="+apiKey+"=31.99663%2C34.82840"
         axios.get(apiUrl).then((res)=>{
               myCity=res.data.LocalizedName;
               }).catch((err)=>{
